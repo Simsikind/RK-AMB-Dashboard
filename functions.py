172924e54376432ba873d2d746ce1921b89899b4
Simon Betzwar <e12402134@student.tuwien.ac.at>
2024-10-11 20:09:02 +0200

    alpa v1

---
+++ functions.py
@@ -0,0 +1,48 @@
+class Patient:
+    def __init__(self, Num):
+        self.Num = Num
+        pass
+
+    def setAlarmt(self, Alarmtime):
+        self.Alarmt = Alarmtime
+        pass
+
+    def setAlarmstr(self, Alarmreason):
+        self.Alarmstr = Alarmreason
+        pass
+
+    def setBOt(self, BOtime):
+        self.BOt = BOtime
+        pass
+
+    def setBOplace(self, BOpl):
+        self.BOplace = BOpl
+        pass
+
+    def setHSTt(self, HSTtime):
+        self.HSTt = HSTtime
+        pass
+
+    def setTransportOrg(self, TrOrg):
+        self.TransportAgency = TrOrg
+        pass
+
+    def setTransOrdert(self, TrOt):
+        self.TransOrdt = TrOt
+        pass
+
+    def setEndt(self, Endt):
+        self.Endt = Endt
+        pass
+
+    def setfinished(self, fin):
+        self.finished = fin
+        pass
+
+    def setNaca(self, Naca):
+        self.Naca = Naca
+        pass
+
+AmbNum = "xxxx/xxxxx-x"
+AmbName = "Ambulanzname"
+AmbDate = "DD:MM:YYYY"
--- main.py
+++ main.py
@@ -1,24 +1,5 @@
-class Patient:
-    def __init__(self, Num):
-        self.Num = Num
-        pass
+import functions
 
-    def setAlarmt(self, Alarmtime):
-        self.Alarmt = Alarmtime
-        pass
-
-    def setAlarmstr(self, Alarmreason):
-        self.Alarmstr = Alarmreason
-        pass
-
-    def setBOt(self, BOtime):
-        self.BOt = BOtime
-        pass
-
-    def setBOplace(self, BOpl):
-        self.BOplace = BOpl
-        pass
-
-    def setNaca(self, Naca):
-        self.Naca = Naca
-        pass
+AmbNum = "xxxx/xxxxx-x"
+AmbName = "Ambulanzname"
+AmbDate = "DD:MM:YYYY"
