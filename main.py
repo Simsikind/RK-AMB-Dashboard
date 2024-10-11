829fec82be2b8698e413c498d3ad38f0e4488618
Simon Betzwar <e12402134@student.tuwien.ac.at>
2024-10-11 19:45:22 +0200

    first version of the main file

---
+++ main.py
@@ -0,0 +1,24 @@
+class Patient:
+    def __init__(self, Num):
+        self.Num = Num
+        pass
+
+    def setAlarmt(self, Alarmtime):
+        self.Alarmt = Alarmtime
+        pass
+
+    def setAlarmstr(self, Alarmreason):
+        self.Alarmstr = Alarmreason
+        pass
+
+    def setBOt(self, BOtime):
+        self.BOt = BOtime
+        pass
+
+    def setBOplace(self, BOpl):
+        self.BOplace = BOpl
+        pass
+
+    def setNaca(self, Naca):
+        self.Naca = Naca
+        pass
