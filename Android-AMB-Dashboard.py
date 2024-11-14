88b321cbaf0b447099dfe17a57145d515d30527b
Simon Betzwar <e12402134@student.tuwien.ac.at>
2024-11-14 19:48:57 +0100

    Version 3

--- Android-AMB-Dashboard.py
+++ Android-AMB-Dashboard.py
@@ -1,3 +1,6 @@
-#run this file with your Python IDE to start AMB-Dashboard
+#Führe diese Datei mit Pydroid aus, um das Dashboard auf Android zu starten
+#Installation von PyDroid: https://play.google.com/store/apps/details?id=ru.iiec.pydroid3
+#Für Windows bitte "AMB_Dashboard.exe verwenden"
+
 import main
 main
